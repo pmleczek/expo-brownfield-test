196784f565af5d990db127618cf4680e6a8282ae
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-10-10 17:04:05 +0200

    feat: statically link pods and include additional pods target

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # expo-brownfield-test
 
-## TODO
+## TODO - iOS
 
 - [ ] Create framework target in the project
 - [ ] Convert framework folder to a group
-- [ ] Add new Pod target
-- [ ] Statically link pods
+- [X] Add new Pod target
+- [X] Statically link pods
 - [ ] Add the bundling script for RN
 - [ ] Add the brownfield entrypoint
 - [ ] Patch ExpoModulesProvider
--- plugins/withIosPlugin.ts
+++ plugins/withIosPlugin.ts
@@ -1,10 +1,33 @@
-import { ConfigPlugin, withXcodeProject } from "expo/config-plugins";
+import {
+  ConfigPlugin,
+  withPodfile,
+  withPodfileProperties,
+} from "expo/config-plugins";
+import { addNewPodsTarget } from "./ios/podfile";
 
-const withIosPlugin: ConfigPlugin = (config) => {
-  return withXcodeProject(config, (config) => {
-    console.log("Test iOS");
+const TARGET_NAME = "BrownfieldApp";
+
+const withPodfilePlugin: ConfigPlugin = (config) => {
+  return withPodfile(config, (config) => {
+    config.modResults.contents = addNewPodsTarget(
+      config.modResults.contents,
+      TARGET_NAME
+    );
     return config;
   });
+};
+
+const withPodfilePropertiesPlugin: ConfigPlugin = (config) => {
+  return withPodfileProperties(config, (config) => {
+    config.modResults["ios.useFrameworks"] = "static";
+    return config;
+  });
+};
+
+const withIosPlugin: ConfigPlugin = (config) => {
+  config = withPodfilePlugin(config);
+  config = withPodfilePropertiesPlugin(config);
+  return config;
 };
 
 export default withIosPlugin;
